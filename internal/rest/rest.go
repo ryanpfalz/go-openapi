bdb829223e23df482d6b1e978f70c6c9a060005e
ryanpfalz <19982322+ryanpfalz@users.noreply.github.com>
2022-11-06 18:00:49 -0600

    initial

---
+++ internal/rest/rest.go
@@ -0,0 +1,76 @@
+package rest
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"go.opentelemetry.io/otel"
+
+	"github.com/ryanpfalz/go-openapi/internal"
+)
+
+const otelName = "github.com/ryanpfalz/go-openapi/internal/rest"
+
+// ErrorResponse represents a response containing an error message.
+type ErrorResponse struct {
+	Error       string            `json:"error"`
+	Validations validation.Errors `json:"validations,omitempty"`
+}
+
+func renderErrorResponse(ctx context.Context, w http.ResponseWriter, msg string, err error) {
+	resp := ErrorResponse{Error: msg}
+	status := http.StatusInternalServerError
+
+	var ierr *internal.Error
+	if !errors.As(err, &ierr) {
+		resp.Error = "internal error"
+	} else {
+		switch ierr.Code() {
+		case internal.ErrorCodeNotFound:
+			status = http.StatusNotFound
+		case internal.ErrorCodeInvalidArgument:
+			status = http.StatusBadRequest
+
+			var verrors validation.Errors
+			if errors.As(ierr, &verrors) {
+				resp.Validations = verrors
+			}
+		case internal.ErrorCodeUnknown:
+			fallthrough
+		default:
+			status = http.StatusInternalServerError
+		}
+	}
+
+	if err != nil {
+		_, span := otel.Tracer(otelName).Start(ctx, "renderErrorResponse")
+		defer span.End()
+
+		span.RecordError(err)
+	}
+
+	// XXX fmt.Printf("Error: %v\n", err)
+
+	renderResponse(w, resp, status)
+}
+
+func renderResponse(w http.ResponseWriter, res interface{}, status int) {
+	w.Header().Set("Content-Type", "application/json")
+
+	content, err := json.Marshal(res)
+	if err != nil {
+		// XXX Do something with the error ;)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	w.WriteHeader(status)
+
+	if _, err = w.Write(content); err != nil { //nolint: staticcheck
+		// XXX Do something with the error ;)
+	}
+}
---
+++ internal/rest/task.go
@@ -0,0 +1,242 @@
+package rest
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/ryanpfalz/go-openapi/internal"
+)
+
+const uuidRegEx string = `[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}`
+
+//go:generate counterfeiter -generate
+
+//counterfeiter:generate -o resttesting/task_service.gen.go . TaskService
+
+// TaskService ...
+type TaskService interface {
+	By(ctx context.Context, args internal.SearchParams) (internal.SearchResults, error)
+	Create(ctx context.Context, params internal.CreateParams) (internal.Task, error)
+	Delete(ctx context.Context, id string) error
+	Task(ctx context.Context, id string) (internal.Task, error)
+	Update(ctx context.Context, id string, description string, priority internal.Priority, dates internal.Dates, isDone bool) error
+}
+
+// TaskHandler ...
+type TaskHandler struct {
+	svc TaskService
+}
+
+// NewTaskHandler ...
+func NewTaskHandler(svc TaskService) *TaskHandler {
+	return &TaskHandler{
+		svc: svc,
+	}
+}
+
+// Register connects the handlers to the router.
+func (t *TaskHandler) Register(r *mux.Router) {
+	r.HandleFunc("/tasks", t.create).Methods(http.MethodPost)
+	r.HandleFunc(fmt.Sprintf("/tasks/{id:%s}", uuidRegEx), t.task).Methods(http.MethodGet)
+	r.HandleFunc(fmt.Sprintf("/tasks/{id:%s}", uuidRegEx), t.update).Methods(http.MethodPut)
+	r.HandleFunc(fmt.Sprintf("/tasks/{id:%s}", uuidRegEx), t.delete).Methods(http.MethodDelete)
+	r.HandleFunc("/search/tasks", t.search).Methods(http.MethodPost)
+}
+
+// Task is an activity that needs to be completed within a period of time.
+// nolint: tagliatelle
+type Task struct {
+	ID          string   `json:"id"`
+	Description string   `json:"description"`
+	Priority    Priority `json:"priority"`
+	Dates       Dates    `json:"dates"`
+	IsDone      bool     `json:"is_done"`
+}
+
+// CreateTasksRequest defines the request used for creating tasks.
+type CreateTasksRequest struct {
+	Description string   `json:"description"`
+	Priority    Priority `json:"priority"`
+	Dates       Dates    `json:"dates"`
+}
+
+// CreateTasksResponse defines the response returned back after creating tasks.
+type CreateTasksResponse struct {
+	Task Task `json:"task"`
+}
+
+func (t *TaskHandler) create(w http.ResponseWriter, r *http.Request) {
+	var req CreateTasksRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		renderErrorResponse(r.Context(), w, "invalid request",
+			internal.WrapErrorf(err, internal.ErrorCodeInvalidArgument, "json decoder"))
+
+		return
+	}
+
+	defer r.Body.Close()
+
+	task, err := t.svc.Create(r.Context(), internal.CreateParams{
+		Description: req.Description,
+		Priority:    req.Priority.Convert(),
+		Dates:       req.Dates.Convert(),
+	})
+	if err != nil {
+		renderErrorResponse(r.Context(), w, "create failed", err)
+
+		return
+	}
+
+	renderResponse(w,
+		&CreateTasksResponse{
+			Task: Task{
+				ID:          task.ID,
+				Description: task.Description,
+				Priority:    NewPriority(task.Priority),
+				Dates:       NewDates(task.Dates),
+			},
+		},
+		http.StatusCreated)
+}
+
+func (t *TaskHandler) delete(w http.ResponseWriter, r *http.Request) {
+	// NOTE: Safe to ignore error, because it's always defined.
+	id, _ := mux.Vars(r)["id"] //nolint: gosimple
+
+	if err := t.svc.Delete(r.Context(), id); err != nil {
+		renderErrorResponse(r.Context(), w, "delete failed", err)
+
+		return
+	}
+
+	renderResponse(w, struct{}{}, http.StatusOK)
+}
+
+// ReadTasksResponse defines the response returned back after searching one task.
+type ReadTasksResponse struct {
+	Task Task `json:"task"`
+}
+
+func (t *TaskHandler) task(w http.ResponseWriter, r *http.Request) {
+	// NOTE: Safe to ignore error, because it's always defined.
+	id, _ := mux.Vars(r)["id"] //nolint: gosimple
+
+	task, err := t.svc.Task(r.Context(), id)
+	if err != nil {
+		renderErrorResponse(r.Context(), w, "find failed", err)
+
+		return
+	}
+
+	renderResponse(w,
+		&ReadTasksResponse{
+			Task: Task{
+				ID:          task.ID,
+				Description: task.Description,
+				Priority:    NewPriority(task.Priority),
+				Dates:       NewDates(task.Dates),
+				IsDone:      task.IsDone,
+			},
+		},
+		http.StatusOK)
+}
+
+// UpdateTasksRequest defines the request used for updating a task.
+// nolint: tagliatelle
+type UpdateTasksRequest struct {
+	Description string   `json:"description"`
+	IsDone      bool     `json:"is_done"`
+	Priority    Priority `json:"priority"`
+	Dates       Dates    `json:"dates"`
+}
+
+func (t *TaskHandler) update(w http.ResponseWriter, r *http.Request) {
+	var req UpdateTasksRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		renderErrorResponse(r.Context(), w, "invalid request",
+			internal.WrapErrorf(err, internal.ErrorCodeInvalidArgument, "json decoder"))
+
+		return
+	}
+
+	defer r.Body.Close()
+
+	// NOTE: Safe to ignore error, because it's always defined.
+	id, _ := mux.Vars(r)["id"] //nolint: gosimple
+
+	err := t.svc.Update(r.Context(), id, req.Description, req.Priority.Convert(), req.Dates.Convert(), req.IsDone)
+	if err != nil {
+		renderErrorResponse(r.Context(), w, "update failed", err)
+
+		return
+	}
+
+	renderResponse(w, &struct{}{}, http.StatusOK)
+}
+
+// SearchTasksRequest defines the request used for searching tasks.
+// nolint: tagliatelle
+type SearchTasksRequest struct {
+	Description *string   `json:"description"`
+	Priority    *Priority `json:"priority"`
+	IsDone      *bool     `json:"is_done"`
+	From        int64     `json:"from"`
+	Size        int64     `json:"size"`
+}
+
+// SearchTasksResponse defines the response returned back after searching for any task.
+type SearchTasksResponse struct {
+	Tasks []Task `json:"tasks"`
+	Total int64  `json:"total"`
+}
+
+func (t *TaskHandler) search(w http.ResponseWriter, r *http.Request) {
+	var req SearchTasksRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		renderErrorResponse(r.Context(), w, "invalid request",
+			internal.WrapErrorf(err, internal.ErrorCodeInvalidArgument, "json decoder"))
+
+		return
+	}
+
+	defer r.Body.Close()
+
+	var priority *internal.Priority
+
+	if req.Priority != nil {
+		res := req.Priority.Convert()
+		priority = &res
+	}
+
+	res, err := t.svc.By(r.Context(), internal.SearchParams{
+		Description: req.Description,
+		Priority:    priority,
+		IsDone:      req.IsDone,
+		From:        req.From,
+		Size:        req.Size,
+	})
+	if err != nil {
+		renderErrorResponse(r.Context(), w, "search failed", err)
+
+		return
+	}
+
+	tasks := make([]Task, len(res.Tasks))
+
+	for i, task := range res.Tasks {
+		tasks[i].ID = task.ID
+		tasks[i].Description = task.Description
+		tasks[i].Priority = NewPriority(task.Priority)
+		tasks[i].Dates = NewDates(task.Dates)
+	}
+
+	renderResponse(w,
+		&SearchTasksResponse{
+			Tasks: tasks,
+			Total: res.Total,
+		}, http.StatusOK)
+}
